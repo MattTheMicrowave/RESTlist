5fde81525d94ad6849bd7c4117f95af93ef9cb07
MattTheMicrowave <mwlluis@hotmail.com>
2016-05-21 19:29:56 -0500

    ver 2.0

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,19 +1,33 @@
 $(document).ready(function() {
-  var sodaForm = $('#soda-form');
-  sodaForm.on('submit', function(event) {
-    event.preventDefault();
-    var value = $(event.currentTarget).find('input[type="text"]').val();
-    $.ajax('/sodas', {
-      method: 'POST',
-      data: {soda: value},
-      success: appendSoda,
-      error: function(err) {
-        console.log('err');
-      }
-});
+    var sodaForm = $('#soda-form');
+    appendSodas();
+    sodaForm.on('submit', function(event) {
+        event.preventDefault();
+        var value = $(event.currentTarget).find('input[type="text"]').val();
+        $.ajax('/sodas', {
+            method: 'POST',
+            data: {
+                soda: value
+            },
+            success: appendSoda,
+            error: function(err) {
+                console.log('err');
+            }
+        });
 
-  });
-  function appendSoda(soda) {
-    $('#sodas-list').append('<li>' + soda.soda + '</li>');
-  }
+    });
+
+    function appendSodas() {
+      $.ajax('/sodas', {
+        success: function(sodas) {
+          sodas.forEach(function(soda) {
+            appendSoda(soda);
+          });
+        }
+      });
+    }
+
+    function appendSoda(soda) {
+        $('#sodas-list').append('<li>' + soda.soda + '</li>');
+    }
 });
