86902ba8c9fd4eadc02dd18ebfb40a866827a1f4
MattTheMicrowave <mwlluis@hotmail.com>
2016-05-25 13:04:12 -0500

    starting backbone

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,71 +1,13 @@
-$(document).ready(function() {
-    var sodaForm = $('#soda-form');
-    appendSodas();
-    sodaForm.on('submit', function(event) {
-        event.preventDefault();
-        var value = $(event.currentTarget).find('input[type="text"]').val();
-        $.ajax('/sodas', {
-            method: 'POST',
-            data: {
-                soda: value
-            },
-            success: function(soda) {
-              appendSoda(soda);
-              addListener();
-            },
-            error: function(err) {
-                console.log('err');
-            }
-        });
+// $(document).ready(function() {
+  var SodaModel = Backbone.Model.extend ({
+      urlRoot: '/sodas',
+      idAttribute: '_id'
+  });
 
-    });
-
-    function addTaskListener() {
-      $('li form').on('submit', function(event){
-        event.preventDefault();
-        var value = $(event.currentTarget).find('input[type="text"]').val();
-        var id = $(event.currentTarget).find('input[type="text"]').data('id');
-        $.ajax('/sodas/' + id, {
-          method: 'PUT',
-          data: { soda : value },
-          success: function(data) {
-            console.log(data);
-          }
-        });
-      });
-    }
-
-    // function addListener() {
-    //   $('li').on('click', function(event) {
-    //     var target = $(event.currentTarget);
-    //     var id = target.data('id');
-    //
-    //     $.ajax('/sodas/' + id, {
-    //       method: 'DELETE',
-    //       success: function(data) {
-    //         target.detach();
-    //       }
-    //     })
-    //   });
-    //
-    // }
+  var SodasCollection = Backbone.Collection.extend({
+      url: '/sodas',
+      model: SodaModel
+  });
 
 
-    function appendSodas() {
-      $.ajax('/sodas', {
-        success: function(sodas) {
-          sodas.forEach(function(soda) {
-            appendSoda(soda);
-          });
-          addListener();
-        }
-      });
-    }
-
-    function appendSoda(soda) {
-        var $li = $('<li></li>');
-        // $li.append('<div class="plain-text">' + soda.soda + '</div>');
-        $li.append('<form><input class="input" type="text" value="' + soda.soda + '" data-id="' + task._id + '"/></form>');
-        $('#sodas-list').append($li);
-    }
-});
+// });
