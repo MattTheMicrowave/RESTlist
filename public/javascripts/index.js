8d339db19592ed9ed0e2959b3c52e49b4cffc8d8
MattTheMicrowave <mwlluis@hotmail.com>
2016-05-25 20:11:01 -0500

    redo of class

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,8 @@
 // $(document).ready(function() {
   var SodaModel = Backbone.Model.extend ({
       urlRoot: '/sodas',
-      idAttribute: '_id'
+      idAttribute: '_id',
+
   });
 
   var SodasCollection = Backbone.Collection.extend({
@@ -9,5 +10,18 @@
       model: SodaModel
   });
 
+var sodasCollection = new SodasCollection();
 
+sodasCollection.fetch({
+    /* inputs is a variable that fetch knows what to do with,
+    it does this by default, 'this' being grab out of the database,
+    so the function grabs out of the database and what it grabs
+    gets consolelogged in this instance */
+    success: function(inputs) {
+        sodasCollection.each(function(inputs) {
+            console.log(inputs.get('soda'));
+        });
+
+    }
+});
 // });
